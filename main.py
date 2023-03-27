4ec456144c71d9fc5805fda4816efed37a23d651
Nichitaking <nichitaking338@gmail.com>
2023-03-27 19:55:09 +0300

    main

---
+++ main.py
@@ -0,0 +1,67 @@
+# try:
+#     print(x)
+# except NameError:
+#     print("Variable x is not defined")
+# except:
+#     print("Something else went wrong")
+
+
+
+# try:
+#     print(x)
+# except NameError:
+#     print("Variable x is not defined")
+# else:
+#     print("Something else went wrong")
+
+
+
+
+#try:
+#     print(x)
+# except:
+#     print("Something else went wrong")
+# finally:
+#     print("The 'try except' is finished")
+
+
+
+#    #Declare a class
+# class Mypet():
+#     #declare paraments
+#     name = 'Rex'
+#     age = 23
+#    #Create a object
+# dog = Mypet()
+#
+# #Print parameters
+# print(dog.name)
+# print(dog.age)
+
+
+
+# # Declare a class
+# class Person:
+#     # declare parameters with __init__
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+#
+# p1 = Person("Ion", 16)
+#
+# print(p1.name)
+# print(p1.age)
+
+
+# Declare a class
+class Person:
+    # declare parameters with __init__
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def say_hi(self):
+      print("Hi my name is ", self.name)
+
+p1 = Person("Ion", 16)
+
+p1.say_hi()
